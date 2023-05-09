b7b62a615fae3ff1209b7bc606613b50be8dae1d
Rajat Saxena <rajat24saxena@gmail.com>
2023-05-09 23:57:14 +0530

    Added method to generate text

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 tokenizer = AutoTokenizer.from_pretrained("lmsys/vicuna-13b-delta-v1.1")
 
 model = AutoModelForCausalLM.from_pretrained("lmsys/vicuna-13b-delta-v1.1")
+
+def generate(text):
+  tokens = tokenizer(text)
+  output = model(tokens)
+  return output
+  
